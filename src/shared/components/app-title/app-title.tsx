18c0f680aab4ad198132dcbe7a15434790599440
Adel Amr <os@oss-macbook-pro.localdomain>
2020-05-13 16:01:01 +0200

    Rename appTitle component classname and add alt to app logo

--- src/shared/components/app-title/app-title.tsx
+++ src/shared/components/app-title/app-title.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { images, titles } from "../../constants/defines";
 import "./app-title.css";
+
 export default function AppTitle() {
   return (
-    <div className="app-title-container">
+    <div className="app-title-container-inner">
       <span id="title-logo">
-        <img src={images.appIcon}></img>
+        <img alt="appLogo" src={images.appIcon}></img>
       </span>
       <span id="title-text">{titles.appTitle}</span>
     </div>
