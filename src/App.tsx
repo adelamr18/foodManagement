45c553f63d916724c50e773b1ebf3b33e9c93620
Adel Amr <os@oss-macbook-pro.localdomain>
2020-05-13 16:01:45 +0200

    Add global font style to application and add forget password route

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import "./App.css";
 import UserLogin from "./user-login/user-login";
 import { Route, Switch } from "react-router-dom";
+import ForgetPassword from "./forget-password/forget-password";
+import { appRoutes } from "./shared/constants/defines";
 function App() {
   return (
     <div className="App">
       <div className="main-content-container">
         <Switch>
-          <Route exact path="/" component={UserLogin} />
+          <Route exact path={appRoutes.defaultPage} component={UserLogin} />
+          <Route path={appRoutes.forgetPassword} component={ForgetPassword} />
         </Switch>
       </div>
     </div>
