e57b888645fffae5cc8655a1cfe0e5fea08cd3b3
Adel Amr <os@oss-macbook-pro.localdomain>
2020-05-13 11:13:33 +0200

    Create defines file to hold all relevant application texts and images

---
+++ src/shared/constants/defines.tsx
@@ -0,0 +1,7 @@
+export const images = {
+  appIcon: require("../../assets/images/appIcon.png"),
+  pizzaBackground: require("../../assets/images/pizzaBackground.png")
+};
+export const titles = {
+  appTitle: "LUNCHTIME"
+};
