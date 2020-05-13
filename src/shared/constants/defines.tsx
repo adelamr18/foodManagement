8ef3f34c36aa1bee210a614e6e2bb20845380a06
Adel Amr <os@oss-macbook-pro.localdomain>
2020-05-13 16:11:00 +0200

    Add more static text and application routes to defines file

--- src/shared/constants/defines.tsx
+++ src/shared/constants/defines.tsx
@@ -2,6 +2,32 @@ export const images = {
   appIcon: require("../../assets/images/appIcon.png"),
   pizzaBackground: require("../../assets/images/pizzaBackground.png")
 };
+
 export const titles = {
   appTitle: "LUNCHTIME"
+};
+
+export const formTitles = {
+  email: "E-mail",
+  password: "Passwort",
+  emailPlaceholder: "Bitte gib deine E-Mail ein",
+  passwordPlaceholder: "Bitte gib dein Passwort ein",
+  login: "ANMELDEN",
+  forgetPassword: "PASSWORT VERGESSEN",
+  confirmEmailPlaceholder: "Bitte bestätige deine E-Mail",
+  resetPassword: "PASSWORT ZURÜCKSETZEN",
+  returnToLogin: "ZURÜCK ZUR ANMELDUNG",
+  loginTitle: "ANMELDUNG"
+};
+
+export const confirmationButtonStyles = {
+  redButton: "red-button",
+  whiteButton: "white-button",
+  whiteButtonText: "white-button-text",
+  redButtonText: "red-button-text"
+};
+
+export const appRoutes = {
+  forgetPassword: "/forgetPassword",
+  defaultPage: "/"
 };
