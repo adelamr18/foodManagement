b5e327b1b98fe04f7e9e4baf220f52645fd6bac2
Adel Amr <os@oss-macbook-pro.localdomain>
2020-05-13 17:40:52 +0200

    Add more styling and and include loginForm to userLogin module

--- src/user-login/user-login.tsx
+++ src/user-login/user-login.tsx
@@ -1,15 +1,30 @@
 import React from "react";
 import AppTitle from "../shared/components/app-title/app-title";
+import LoginForm from "../shared/components/login-form/login-form";
+import { formTitles, appRoutes } from "../shared/constants/defines";
 import "./user-login.css";
+
 export default function UserLogin() {
   return (
     <div className="login-container">
       <div className="login-first-subcontainer">
-        <div className="app-title">
+        <div className="app-title-outer-container">
           <AppTitle />
         </div>
+        <span id="login-form-title">{formTitles.loginTitle}</span>
       </div>
-      <div className="login-form-container"></div>
+      <div className="login-form-container-outer">
+        <LoginForm
+          firstLabel={formTitles.email}
+          secondLabel={formTitles.password}
+          firstPlaceholderText={formTitles.emailPlaceholder}
+          secondPlaceholderText={formTitles.passwordPlaceholder}
+          firstButtonText={formTitles.login}
+          secondButtonText={formTitles.forgetPassword}
+          navigateToFirstButtonRoute={appRoutes.defaultPage}
+          navigateToSecondButtonRoute={appRoutes.forgetPassword}
+        />
+      </div>
       <div className="login-second-subcontainer"></div>
     </div>
   );
